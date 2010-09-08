94bf234cdb3493562eefcfa312c7abb4f8fe2817
James Ekow Abaka Ainooson <jainooson@gmail.com>
2010-09-08 16:11:43 +0000

    Authenticators have now been rewritten so that the Auth class performs the actual role of changing routes provided all authentication credentials were properly provided to the AuthMethod class

--- controllers/components/auth/Auth.php
+++ controllers/components/auth/Auth.php
@@ -1,21 +1,61 @@
 <?php
+/**
+ * The authentication component base class's script file.
+ *
+ * LICENSE:
+ * Copyright 2010 James Ekow Abaka Ainooson
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @package    ntentan.controllers.components
+ * @author     James Ekow Abaka Ainooson <jainooson@gmail.com>
+ * @copyright  2010 James Ekow Abaka Ainooson
+ * @license    http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 namespace ntentan\controllers\components\auth;
 
-use \ntentan\Ntentan;
+use ntentan\Ntentan;
 use \ntentan\controllers\components\Component;
 
 /**
+ * The class for the authentication component. This class provides an entry point
+ * through which the various authentication methods could be used. It also acts
+ * as a watch dog of some sort which prevents un authenticated users from 
+ * acessing sections of the site they are not entitled to. Finally this class
+ * provides a role mechanism so that the various users of the system can be limited
+ * to what they have access to based on thei attached role.
  * 
+ * @author James Ekow Abaka Ainooson <jainooson@gmail.com>
+ * @package ntentan.controllers.components.auth
  */
 class Auth extends Component
 {
+    /**
+     * The URL route through wich the login component should be invoked. This 
+     * path should point to a controller which exists and implements the required
+     * method.
+     * @var string
+     */
     public $loginPath = "users/login";
+    
+    
     public $logoutPath = "users/logout";
     public $redirectPath = "/";
     public $redirectOnSuccess = true;
     public $name = __CLASS__;
     public $authMethod = "http_request";
-    public $usersModel = "asembisa.users";
+    public $usersModel = "users";
     protected $authMethodInstance;
     
     public function preRender()
@@ -25,7 +65,7 @@ class Auth extends Component
         if(class_exists($authenticatorClass))
         {
             $this->authMethodInstance = new $authenticatorClass();
-            $this->authMethodInstance->setAuthComponent($this);
+            $this->authMethodInstance->usersModel = $this->usersModel;
         }
         else
         {
@@ -35,6 +75,7 @@ class Auth extends Component
         // Allow the roles component to activate the authentication if it is
         // available. If not just run the authenticator from this section.
         if($this->controller->hasComponent("roles")) return;
+        
         if($_SESSION["logged_in"] === false || !isset($_SESSION["logged_in"]))
         {
             $this->login();
@@ -43,12 +84,31 @@ class Auth extends Component
     
     public function login()
     {
-        $this->authMethodInstance->login();
+        if($this->authMethodInstance->login())
+        {
+            if($this->redirectOnSuccess)
+            {
+                Ntentan::redirect($this->redirectPath);
+            }
+            else
+            {
+                $this->set("login_status", true);
+            }
+        }
+        else
+        {
+            $this->set("login_message", $this->authMethodInstance->message);
+            $this->set("login_status", false);
+            if(Ntentan::$route != $this->loginPath)
+            {
+                Ntentan::redirect($this->loginPath);
+            }
+        }
     }
 
     public function logout()
     {
         $_SESSION = array();
-        Ntentan::redirect($this->redirectPath);
+        Ntentan::redirect($this->loginPath);
     }
 }
--- controllers/components/auth/HttpRequest.php
+++ controllers/components/auth/HttpRequest.php
@@ -10,36 +10,11 @@ class HttpRequest extends AuthMethod
     {
         if(isset($_REQUEST["username"]) && isset($_REQUEST["password"]))
         {
-            $usersModelClass = Model::getClassName($this->auth->usersModel);
-            $users = new $usersModelClass();
-            $result = $users->getFirstWithUsername($_REQUEST["username"]);
-            if($result->password == md5($_REQUEST["password"]))
-            {
-                $_SESSION["logged_in"] = true;
-                $_SESSION["username"] = $_REQUEST["username"];
-                $_SESSION["user_id"] = $result["id"];
-                $this->auth->set("login_message", "Successful login" );
-                $this->auth->set("login_status", true);
-                if($this->auth->redirectOnSuccess === true) 
-                {
-                    Ntentan::redirect( 
-                        Ntentan::getUrl($this->auth->redirectPath), 
-                        true
-                  );
-                }
-            }
-            else
-            {
-                $this->auth->set("login_message", "Invalid username or password" );
-                $this->auth->set("login_status", false);
-            }
+            return $this->authLocalPassword($_REQUEST["username"], $_REQUEST["password"]);
         }
         else
         {
-            if(Ntentan::$route != $this->auth->loginPath && Ntentan::$route != $this->auth->logoutPath) 
-            {
-                Ntentan::redirect($this->auth->loginPath);
-            }
-        }       
+            return false;
+        }
     }
 }
