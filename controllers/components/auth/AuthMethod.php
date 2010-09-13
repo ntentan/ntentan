217748c7313e8eb217a1ee23052ab3b0e222343d
James Ekow Abaka Ainooson <jainooson@gmail.com>
2010-09-13 07:09:30 +0000

    Authenticators have now been rewritten so that the Auth class performs the actual role of changing routes provided all authentication credentials were properly provided to the AuthMethod class
    git-svn-id: https://ntentan.googlecode.com/svn/trunk@56 05a48904-4b55-0410-ac7d-d94522421892

--- controllers/components/auth/AuthMethod.php
+++ controllers/components/auth/AuthMethod.php
@@ -1,12 +1,32 @@
 <?php
 namespace ntentan\controllers\components\auth;
 
+use \ntentan\models\Model;
+
 abstract class AuthMethod
 {
-    protected $auth;
+    public $usersModel;
+    public $redirectPath;
+    public $message;
     
-    public function setAuthComponent($auth)
+    abstract public function login();
+    
+    public function authLocalPassword($username, $password)
     {
-        $this->auth = $auth;
+        $usersModelClass = Model::getClassName($this->usersModel);
+        $users = new $usersModelClass();
+        $result = $users->getFirstWithUsername($username);
+        if($result->password == md5($password))
+        {
+            $_SESSION["logged_in"] = true;
+            $_SESSION["username"] = $username;
+            $_SESSION["user_id"] = $result["id"];
+            return true;
+        }
+        else
+        {
+            $this->message = "Invalid username or password!";
+            return false;
+        }
     }
 }
