01d3b8b9bde30c62e710083287d670ce752fb852
Ekow Abaka Ainooson <jainooson@gmail.com>
2014-12-15 11:26:59 +0000

    Fixed a bug in the admin edit section

--- lib/controllers/components/admin/views/templates/admin_component_edit.tpl.php
+++ lib/controllers/components/admin/views/templates/admin_component_edit.tpl.php
@@ -3,6 +3,7 @@
 echo $console_menu_block;
 $this->helpers->form->setErrors($errors);
 $this->helpers->form->setData($data);
+echo $this->helpers->form->open("add-$entity-form");
 $form = t(
     "edit_{$entity_code}_form.tpl.php", 
     array(
@@ -11,6 +12,5 @@ $form = t(
         'variables' => $form_variables
     )
 );
-echo $this->helpers->form->open("add-$entity-form");
 echo $form;
 echo $this->helpers->form->close('Update');
