d1f38d867c7d1b80d621e305cf0610546e567a7c
Ekow Abaka Ainooson <jainooson@gmail.com>
2015-08-05 07:57:35 +0000

    Cleaning up for a new direction ....

--- tests/app/modules/departments/Departments.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace ntentan_test_app\modules\departments;
-
-class Departments extends \ntentan\models\Model
-{
-    public $hasMany = array(
-        'users'
-    );
-}
--- tests/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-error_reporting(E_ALL ^ E_NOTICE);
-
-define ("TEST_HOME", __DIR__);
-require "vendor/autoload.php";
-
