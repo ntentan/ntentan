e87103749ea728d10c290de69eaa34be6d098d54
James Ainooson <jainooson@gmail.com>
2017-03-08 12:59:11 -0600

    Added some documentation and a getName() method to Models

--- src/Model.php
+++ src/Model.php
@@ -3,10 +3,29 @@ namespace ntentan;
 
 use ntentan\nibii\RecordWrapper;
 
+/**
+ * An extension of the nibii\RecordWrapper which contains specific Ntentan
+ * extensions.
+ */
 class Model extends RecordWrapper
 {
+    /**
+     * Loads a model described by a string.
+     * @param string $name
+     * @return \ntentan\Model
+     */
     public static function load($name)
     {
         return nibii\Nibii::load($name);
+    }
+    
+    /**
+     * Get a descriptive name for the model.
+     * Names are usually deduced from the class name of the underlying model.
+     * @return string
+     */
+    public function getName()
+    {
+        return (new \ReflectionClass($this))->getShortName();
     }
 }
