dbee829cd791c8b2d16944e7fb7744394a6287cb
James Ainooson <jainooson@gmail.com>
2017-08-23 22:05:52 -0500

    Redirect now takes the get url and redirects to it

--- src/Redirect.php
+++ src/Redirect.php
@@ -2,18 +2,32 @@
 
 namespace ntentan;
 
+use ntentan\utils\Input;
+
 /**
  * Sets headers for redirecting.
+ * To perform a redirection, a view can return an instance of this class. Utility methods exist in the Controller
+ * class to generate Redirect objects which are automatically initialized with the URL of the controller. A redirect
+ * URL query passed with the current URL overrides any URL passed to this class.
  *
  * @author ekow
  */
 class Redirect
 {
+    /**
+     * URL to redirect to.
+     * 
+     * @var string
+     */
     private $url;
 
+    /**
+     * Create new instance of Redirect.
+     * @param string $url The target URL of the redirection.
+     */
     public function __construct($url)
     {
-        $this->url = $url;
+        $this->url = Input::get("redirect") ?? $url ;
     }
 
     public function __toString()
