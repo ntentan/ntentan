3b454fdbaddb09b3f4fbc5c3c237ce060e7e38b8
Ekow Abaka Ainooson <jainooson@gmail.com>
2020-03-06 07:23:41 -0600

    Uploaded file binder now supports camelcase attributes

--- src/controllers/model_binders/UploadedFileBinder.php
+++ src/controllers/model_binders/UploadedFileBinder.php
@@ -5,6 +5,7 @@ namespace ntentan\controllers\model_binders;
 use ntentan\controllers\ModelBinderInterface;
 use ntentan\utils\filesystem\UploadedFile;
 use ntentan\Controller;
+use ntentan\utils\Text;
 
 /**
  *
@@ -14,7 +15,12 @@ class UploadedFileBinder implements ModelBinderInterface
 {
     public function bind(Controller $controller, string $type, string $name, array $parameters, $instance = null)
     {
-        return isset($_FILES[$name]) ? new UploadedFile($_FILES[$name]) : null;
+        if(isset($_FILES[$name])) {
+            return new UploadedFile($_FILES[$name]);
+        } elseif (isset($_FILES[$decamelizedName = Text::deCamelize($name)])) {
+            return new UploadedFile($_FILES[$decamelizedName]);
+        }
+        return null;
     }
     
     public function requiresInstance() : bool
