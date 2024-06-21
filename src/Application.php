485777434301a81c3b7997e961b908c76f566d35
James Ainooson <jainooson@gmail.com>
2024-06-20 20:18:56 -0500

    Cleaning up and adding standard interfaces

--- src/Application.php
+++ src/Application.php
@@ -33,59 +33,54 @@ class Application
     protected $middlewareFactoryRegistry;
 
     /**
-     *
-     * @param Router $router
-     * @param Config $config
-     * @param PipelineRunner $runner
-     * @param Cache $cache
-     * @param SessionContainerFactory $sessionContainerFactory
+     * 
      */
-    public final function __construct(Context $context, Router $router, Config $config, PipelineRunner $runner, Cache $cache, SessionContainerFactory $sessionContainerFactory)
+    public final function __construct(SessionContainerFactory $sessionContainerFactory)
     {
-        $this->context = $context;
-        $this->router = $router;
-        $this->config = $config;
-        $this->runner = $runner;
-        $this->cache = $cache;
+//        $this->context = $context;
+//        $this->router = $router;
+//        $this->config = $config;
+//        $this->runner = $runner;
+//        $this->cache = $cache;
         $this->sessionContainerFactory = $sessionContainerFactory;
-        $this->prefix = $config->get('app.prefix') ?? "";
+//        $this->prefix = $config->get('app.prefix') ?? "";
     }
 
-    protected function setup() : void
-    {
-        $routes = require "bootstrap/routes.php";
-        $this->router->setRoutes($routes['routes']);
-        $this->defaultPipeline = $routes['default_pipeline'];
-    }
+//    protected function setup() : void
+//    {
+//        $routes = require "bootstrap/routes.php";
+//        $this->router->setRoutes($routes['routes']);
+//        $this->defaultPipeline = $routes['default_pipeline'];
+//    }
+//
+//    public function setDatabaseDriverFactory(DriverFactory $driverFactory) : void
+//    {
+//        DbContext::initialize($driverFactory);
+//    }
+//
+//    public function setOrmFactories(ModelFactoryInterface $modelFactory, DriverAdapterFactoryInterface $driverAdapterFactory, ValidatorFactoryInterface $modelValidatorFactory) : void
+//    {
+//        ORMContext::initialize($modelFactory, $driverAdapterFactory, $modelValidatorFactory, $this->cache);
+//    }
+//    
+//    public function setMiddlewareFactoryRegistry(MiddlewareFactoryRegistry $middlewareFactoryRegistry)
+//    {
+//        $this->middlewareFactoryRegistry = $middlewareFactoryRegistry;
+//    }
+//    
+//    private function buildPipeline($pipeline)
+//    {
+//        $instances = [];
+//        foreach($pipeline as $middleware) {
+//            $instance = $this->middlewareFactoryRegistry->getFactory($middleware[0])->createMiddleware($middleware[1] ?? []);
+//            $instances[] = $instance;
+//        }
+//        return $instances;
+//    }
 
-    public function setDatabaseDriverFactory(DriverFactory $driverFactory) : void
+    public function execute(array $pipeline)
     {
-        DbContext::initialize($driverFactory);
-    }
-
-    public function setOrmFactories(ModelFactoryInterface $modelFactory, DriverAdapterFactoryInterface $driverAdapterFactory, ValidatorFactoryInterface $modelValidatorFactory) : void
-    {
-        ORMContext::initialize($modelFactory, $driverAdapterFactory, $modelValidatorFactory, $this->cache);
-    }
-    
-    public function setMiddlewareFactoryRegistry(MiddlewareFactoryRegistry $middlewareFactoryRegistry)
-    {
-        $this->middlewareFactoryRegistry = $middlewareFactoryRegistry;
-    }
-    
-    private function buildPipeline($pipeline)
-    {
-        $instances = [];
-        foreach($pipeline as $middleware) {
-            $instance = $this->middlewareFactoryRegistry->getFactory($middleware[0])->createMiddleware($middleware[1] ?? []);
-            $instances[] = $instance;
-        }
-        return $instances;
-    }
-
-    public function execute()
-    {
-        $this->setup();
+//        $this->setup();
         $this->sessionContainerFactory->createSessionContainer();
         $route = $this->router->route(substr(parse_url(filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_URL), PHP_URL_PATH), 1), $this->prefix);
         $this->context->setParameter('route', $route['route']);
