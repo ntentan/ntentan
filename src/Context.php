6a11b5494f6cd40edea58563c117474a0bec2591
James Ekow Abaka Ainooson <jainooson@gmail.com>
2026-06-09 06:56:22 -0500

    feat: Adding a way to get the ip address from the context

--- src/Context.php
+++ src/Context.php
@@ -34,4 +34,32 @@ class Context
     {
         return $this->session;
     }
+
+    public function getIpAddress(): string
+    {
+        $ip_keys = [
+            'HTTP_CLIENT_IP',
+            'HTTP_X_FORWARDED_FOR',
+            'HTTP_X_FORWARDED',
+            'HTTP_X_CLUSTER_CLIENT_IP',
+            'HTTP_FORWARDED_FOR',
+            'HTTP_FORWARDED',
+            'REMOTE_ADDR'
+        ];
+
+        foreach ($ip_keys as $key) {
+            if (array_key_exists($key, $_SERVER) === true) {
+                // Headers can contain comma-separated lists of IPs if passed through multiple proxies
+                foreach (explode(',', $_SERVER[$key]) as $ip) {
+                    $ip = trim($ip);
+
+                    // Validate that it is a proper IP format (v4 or v6)
+                    if (filter_var($ip, FILTER_VALIDATE_IP) !== false) {
+                        return $ip;
+                    }
+                }
+            }
+        }
+        return '0.0.0.0';
+    }
 }
